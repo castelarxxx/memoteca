2ebab9598484dce158f56d1c18cdd273a5c751fa
Hilary Castelar <hilary.castelar555@gmail.com>
2024-08-24 22:57:44 -0300

    feat: implementando interfaces

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,22 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos =[
-    {
-    conteudo: 'Passo informações para o componentefilho',
-    autoria: 'Pai component',
-    modelo: 'modelo2'
-     },
-     {
-      conteudo: 'Minha propriedade é decorada com @Input',
-      autoria: 'FIlho component',
-      modelo: 'modelo3'
-       },
-       {
-        conteudo: 'Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Diuretics paradis num copo é motivis de denguis. Atirei o pau no gatis, per gatis num morreus. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Sapien in monti palavris qui num significa nadis i pareci latim.',
-        autoria: '',
-        modelo: 'modelo1'
-    },
+  listaPensamentos: Pensamento []=[
+
   ];
   constructor() { }
 
