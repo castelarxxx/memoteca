112a2079e3c958d5accf81d5d72aadc733839a7f
Hilary Castelar <hilary.castelar555@gmail.com>
2024-10-22 18:11:55 -0300

    feat: evoluindo aplicação

--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,31 +1,47 @@
+import { Pensamento } from './../pensamento';
 import { Component, Input, OnInit } from '@angular/core';
-import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-pensamento',
   templateUrl: './pensamento.component.html',
-  styleUrls: ['./pensamento.component.css']
+  styleUrls: ['./pensamento.component.css'],
 })
 export class PensamentoComponent implements OnInit {
-
-
- @Input() pensamento: Pensamento = {
-    id:0,
+  @Input() pensamento: Pensamento = {
+    id: 0,
     conteudo: 'I love Angular',
-    autoria: 'Hilary',
-    modelo: 'modelo2'
-}
+    autoria: 'Nay',
+    modelo: 'modelo3',
+    favorito: false,
+  };
 
-  constructor() { }
+  @Input() listaFavoritos: Pensamento[] = [];
 
-  ngOnInit(): void {
-  }
+  constructor(private service: PensamentoService) {}
 
+  ngOnInit(): void {}
 
   larguraPensamento(): string {
-    if(this.pensamento.conteudo.length >= 256) {
-      return 'pensamento-g'
+    if (this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g';
     }
-    return 'pensamento-p'
+    return 'pensamento-p';
+  }
+
+  mudarIconeFavorito(): string {
+    if (this.pensamento.favorito == false) {
+      return 'inativo';
+    }
+    return 'ativo';
+  }
+
+  atualizarFavoritos() {
+    this.service.mudarFavorito(this.pensamento).subscribe(() => {
+      this.listaFavoritos.splice(
+        this.listaFavoritos.indexOf(this.pensamento),
+        1
+      );
+    });
   }
 }
